104c49461421112cc6159e16ce823b3f718b41c6
Chunhui Gu <fduguchunhui@gmail.com>
2025-02-14 13:25:43 -0600

    Add functionality to allow input qvalue and queue config

--- create_job.sh
+++ create_job.sh
@@ -1,122 +1,118 @@
 #!/bin/bash
 
-# # Define the root working directory for shell script, use absolute path, every other path should be specified as relative path to this one
-# working_directory="/rsrch5/scratch/ccp/hanash/Hanash_GPFS/Chunhui"
-# # Default values for arguments
-# file_folder="DIANN_Testing/data/BrC_CtrlF"
-# library_file="DIANN_Testing/library/UNIPROT2301_SP_HUMAN_Hypusine.predicted.speclib"
-# # For citrullination "--unimod4 --var-mods 5 --var-mod UniMod:35,15.994915,M --relaxed-prot-inf --rt-profiling --var-mod Citrullination,0.984016,R"
-# For Phosophrolation STY ""
-# ptm_params="--unimod4 --var-mods 5 --relaxed-prot-inf --rt-profiling --var-mod Hypusine,87.068414,K --var-mod Deoxyhypusine,71.073499,K"
+# Default values for arguments
+working_directory="/rsrch5/scratch/ccp/hanash/Hanash_GPFS/Chunhui"
+queue="long"
+num_threads=28
+min_memory=100
+max_memory=350
+wall_time="240:00"
+singularity_image="diann/diann-1.9.2.img"
+temp_directory="DIANN_Testing/temp"
+email="cgu3@mdanderson.org"
+qvalue=0.01  # Default Q-value
+output_name=""  # Default output name (set per file if not provided)
 
 # Function to display help message
 show_help() {
-  echo "Usage: $0 [-r working_directory] [-f file_folder] [-l library_file] [-p ptm_params]"
+  echo "Usage: $0 [-r working_directory] [-f file_folder] [-l library_file] [-p ptm_params] [-q queue] [-n num_threads] [-M min_memory] [-W wall_time] [-v qvalue] [-o output_name]"
   echo
   echo "Options:"
-  echo "  -r  Set the working (root) directory (default: /rsrch5/scratch/ccp/hanash/Hanash_GPFS/Chunhui), everything else should be relative path to it"
-  echo "  -m  Set minimal memory required"
-  echo "  -f  Set the spectrum data folder (default: DIANN_Testing/BrC_CtrlF)"
-  echo "  -l  Set the library file (default: DIANN_Testing/library/UNIPROT2301_SP_HUMAN_Hypusine.predicted.speclib)"
-  echo "  -p  Set the PTM parameters (default: --unimod4 --var-mods 5 --relaxed-prot-inf --rt-profiling --var-mod Hypusine,87.068414,K --var-mod Deoxyhypusine,71.073499,K)"
+  echo "  -r  Set the working (root) directory (default: $working_directory)"
+  echo "  -m  Set minimal memory required (default: $min_memory MB)"
+  echo "  -f  Set the spectrum data folder (required)"
+  echo "  -l  Set the library file (required)"
+  echo "  -p  Set the PTM parameters (required)"
+  echo "  -q  Set the job queue (default: $queue)"
+  echo "  -n  Set the number of threads (default: $num_threads)"
+  echo "  -M  Set the max memory usage per job (default: $max_memory MB)"
+  echo "  -W  Set the wall time limit (default: $wall_time)"
+  echo "  -v  Set the q-value for DIA-NN (default: $qvalue)"
+  echo "  -o  Set a custom output name (default: same as file_folder name)"
   echo "  -h  Show this help message"
 }
 
 # Parse command-line arguments
-while getopts "hr:m:f:l:p:" opt; do
+while getopts "hr:m:f:l:p:q:n:M:W:v:o:" opt; do
   case $opt in
     r) working_directory="$OPTARG" ;;
     m) min_memory="$OPTARG" ;;
     f) file_folder="$OPTARG" ;;
     l) library_file="$OPTARG" ;;
     p) ptm_params="$OPTARG" ;;
+    q) queue="$OPTARG" ;;
+    n) num_threads="$OPTARG" ;;
+    M) max_memory="$OPTARG" ;;
+    W) wall_time="$OPTARG" ;;
+    v) qvalue="$OPTARG" ;;  # Assign q-value
+    o) output_name="$OPTARG" ;;  # Assign custom output name
     h) show_help; exit 0 ;;
     \?) echo "Invalid option -$OPTARG" >&2; exit 1 ;;
   esac
 done
 
 # Check if required arguments are provided
-if [ -z "$working_directory" ] || [ -z "$file_folder" ] || [ -z "$library_file" ] || [ -z "$ptm_params" ]; then
-  echo "Error: All arguments -r, -f, -l, and -p are required."
+if [[ -z "$file_folder" || -z "$library_file" || -z "$ptm_params" ]]; then
+  echo "Error: Arguments -f (file_folder), -l (library_file), and -p (ptm_params) are required."
   show_help
   exit 1
 fi
 
-singularity_image="diann/diann-1.9.2.img"
-temp_directory="DIANN_Testing/temp"
-num_threads=56
-email="cgu3@mdanderson.org"
+# Set default output_name if not provided
+if [[ -z "$output_name" ]]; then
+  output_name="${file_folder##*/}"
+fi
 
-
-# Change to the working directory and print the current directory
+# Change to the working directory
 cd "$working_directory" || { echo "Failed to change directory to $working_directory"; exit 1; }
-pwd  # This will print the current working directory
+pwd
 
-# Ensure the tasks directories exist
-mkdir -p "$working_directory"/"$lsf_directory"
-
-basename=${library_file##*/}       # Remove the directory part
-library_name=${basename%%.*}             # Remove the file extension
-file_folder_name=${file_folder##*/}
-task_name="${file_folder_name}_${library_name}"
-# combine file_folder_name with library_name
-output_directory="DIANN_Testing/output/$task_name"
-# Define the directory to save created LSF script
-lsf_directory="diann/tasks/$task_name"
+# Modify lsf_directory to use output_name instead of file_folder
+lsf_directory="diann/tasks/${output_name}/${library_file##*/}"  
 mkdir -p "$lsf_directory"
+mkdir -p "$working_directory/$lsf_directory"
 
 # Loop through each .d file in the specified directory
 for file in "$file_folder"/*.d; do
-    # Extract the base filename without path
     filename=$(basename "$file")
-    # create output sub-directory for each file
-    output_sub_dir="$output_directory"/"${filename%.d}"
-    temp_sub_dir="$temp_directory"/"${filename%.d}"
-    # Check file size
+    
+    output_sub_dir="DIANN_Testing/output/${output_name}/${library_file##*/}/${filename%.d}"
+    temp_sub_dir="$temp_directory/${filename%.d}"
     file_size=$(du -sh "$file" | cut -f1)
-    # Create a job script for each file
+    
     job_script="
 #BSUB -cwd \"$working_directory\"
-
-mkdir -p \"$output_sub_dir\"
-mkdir -p \"$temp_sub_dir\"
-# overwrite log instead of appending
-rm "$output_sub_dir/std_out.txt"
-rm "$output_sub_dir/std_err.txt"
-touch "$output_sub_dir/std_out.txt"
-touch "$output_sub_dir/std_err.txt"
-
 #BSUB -J diann_${filename%.d}
 #BSUB -o \"$output_sub_dir/std_out.txt\"
 #BSUB -e \"$output_sub_dir/std_err.txt\"
-#BSUB -W 240:00
-#BSUB -q long
-#BSUB -n 28
-#BSUB -M 350
+#BSUB -W $wall_time
+#BSUB -q $queue
+#BSUB -n $num_threads
+#BSUB -M $max_memory
 #BSUB -R \"rusage[mem=${min_memory}]\"
-# Don't send email when job start to avoid too many emails when run in batch
-##BSUB -B 
-# Don't send email when job end
-##BSUB -N
 #BSUB -u \"$email\"
 
-# debug information
-echo \"current working directory\"
+echo \"Current working directory\"
 pwd
-echo \"raw data size (mb): $file_size\"
+echo \"Raw data size (mb): $file_size\"
 
-# Run DIANN
+mkdir -p \"$output_sub_dir\"
+mkdir -p \"$temp_sub_dir\"
+rm -f \"$output_sub_dir/std_out.txt\"
+rm -f \"$output_sub_dir/std_err.txt\"
+touch \"$output_sub_dir/std_out.txt\"
+touch \"$output_sub_dir/std_err.txt\"
+
 singularity exec --bind \"${working_directory}:/mnt\" \"${singularity_image}\" /diann-1.9.2/diann-linux \\
---f /\"mnt/${file_folder}/${filename}\" \\
---lib \"/mnt/${library_file}\" \\
+--f /mnt/${file_folder}/${filename} \\
+--lib /mnt/${library_file} \\
 --threads ${num_threads} --verbose 1 \\
---out \"/mnt/${output_sub_dir}/${filename%.d}_report.tsv\" --qvalue 0.01 --matrices \\
---out-lib \"/mnt/${output_sub_dir}/${filename%.d}_report-lib.parquet\" --qvalue 0.01 --matrices \\
---temp \"/mnt/${temp_sub_dir}\" --gen-spec-lib \\
-\"${ptm_params}\"
+--out /mnt/${output_sub_dir}/${filename%.d}_report.tsv --qvalue ${qvalue} --matrices \\
+--out-lib /mnt/${output_sub_dir}/${filename%.d}_report-lib.parquet \\
+--temp /mnt/${temp_sub_dir} --gen-spec-lib \\
+${ptm_params}
 "
 
-    # Write the job script to a file
     echo "$job_script" > "$lsf_directory/${filename%.d}.lsf"
     echo "$lsf_directory/${filename%.d}.lsf created"
 done
